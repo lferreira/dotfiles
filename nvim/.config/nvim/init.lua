fad0295718097eb5d37678e1112867292fd0a93a
luiz.ferreira <luizhgferreira@gmail.com>
2023-06-17 10:08:15 +0200

    introducing nvim - simple config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,12 @@
+require('plugins')
+require('file-explorer')
+require('mason-config')
+require('gopls')
+require('code-completion')
+require('custom-keys')
+require('styling')
+require('file-finder')
+require('syntax-highlight')
+require('statusbar')
+
+vim.wo.number = true

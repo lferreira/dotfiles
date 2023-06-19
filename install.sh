124208570304b07766b3e8ac42f08bbee5fd05fb
luiz.ferreira <luizhgferreira@gmail.com>
2023-06-19 09:23:07 +0200

    uses gopls as formatter

--- install.sh
+++ install.sh
@@ -10,9 +10,10 @@ mkdir ~/.config
 # creates a symbolyc link with ~/.config/nvim
 ln -s ~/.dotfiles/nvim ~/.config/nvim
 
-go install github.com/incu6us/goimports-reviser/v3@latest 
+go install github.com/incu6us/goimports-reviser/v3@latest
 go install mvdan.cc/gofumpt@latest
 go install github.com/segmentio/golines@latest
+go install golang.org/x/tools/gopls@latest
 
 vim +PlugInstall +qall
 

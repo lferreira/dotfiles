70223299a2c2588c7f89d63d8965bb03283020bc
luiz.ferreira <luizhgferreira@gmail.com>
2023-06-19 09:30:33 +0200

    adds which key

--- nvim/lua/luiz/plugins-setup.lua
+++ nvim/lua/luiz/plugins-setup.lua
@@ -50,6 +50,20 @@ return packer.startup(function(use)
 
 	use("szw/vim-maximizer") -- maximizes and restores current window
 
+	-- which key
+	use({
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+			require("which-key").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	})
+
 	-- Telescope used to fuzzy search files
 	-- Lualine information / Status bar
 	use({

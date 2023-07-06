5ebb8d0e45fffca6380daa4aa1d0ada0e9c2f4c6
luiz.ferreira <luizhgferreira@gmail.com>
2023-07-06 16:18:47 +0200

    introduces yabai

--- install.sh
+++ install.sh
@@ -24,7 +24,42 @@ xcode-select --install
 brew update
 
 # installing git
-brew install git to clone dotfiles
+brew install git
+
+# install password store
+git clone git@github.com:lferreira/pwd-store.git ~/.password-store
+git clone git@github.com:lferreira/pwd-keys.git
+
+#  Remember to edit the private key gpg --edit-key <private-key>
+#  According to this post, OpenKeyChain doesn't suport AEAD encryption
+#  (default in gpg 2.3). Dropping the OCB option from the key's preference
+#  string also fixed the issue for me:
+#
+#  gpg --edit-key <key-id>
+#	 gpg> showpref
+#	 [ultimate] (1).name <email>
+#	      Cipher: AES256, AES192, AES, 3DES
+#	      AEAD: OCB
+#	      Digest: SHA512, SHA384, SHA256, SHA224, SHA1
+#	      Compression: ZLIB, BZIP2, ZIP, Uncompressed
+#	      Features: MDC, AEAD, Keyserver no-modify
+#	 gpg> setpref AES256 AES192 AES 3DES SHA512 SHA384 SHA256 SHA224 SHA1 ZLIB BZIP2 ZIP
+#	 Set preference list to:
+#	      Cipher: AES256, AES192, AES, 3DES
+#	      AEAD:
+#	      Digest: SHA512, SHA384, SHA256, SHA224, SHA1
+#	      Compression: ZLIB, BZIP2, ZIP, Uncompressed
+#	      Features: MDC, Keyserver no-modify
+#	 Really update the preferences? (y/N) y
+#	 gpg> save
+#  pass init <key-id>
+
+gpg --import ~/pwd-keys/private.pgp
+gpg --import ~/pwd-keys/public.pgp
+rm -rf ~/pwd-keys
+
+# allow changes on the new machine - adds trust|5|save
+gpg --edit-key luizhgferreira@gmail.com
 
 cd "$(dirname "${BASH_SOURCE}")";
 
@@ -47,6 +82,12 @@ ln -s ~/.dotfiles/tmux ~/.config/tmux
 
 # create a symbolyc link to ~/.config/tmate
 ln -s ~/.dotfiles/tmate ~/.config/tmate
+
+# create a symbolyc link to ~/.config/skhd
+ln -s ~/.dotfiles/skhd ~/.config/skhd
+
+# create a symbolyc link to ~/.config/yabai
+ln -s ~/.dotfiles/skhd ~/.config/yabai
 
 echo "Running Brew ..."
 
@@ -84,37 +125,8 @@ vim +PlugInstall +qall
 echo "Installing Neovim plugins ..."
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
-# install password store
-git clone git@github.com:lferreira/pwd-store.git ~/.password-store
-git clone git@github.com:lferreira/pwd-keys.git
+# starting skhd
+skhd --start-service
 
-#  Remember to edit the private key gpg --edit-key <private-key>
-#  According to this post, OpenKeyChain doesn't suport AEAD encryption
-#  (default in gpg 2.3). Dropping the OCB option from the key's preference
-#  string also fixed the issue for me:
-#
-#  gpg --edit-key <key-id>
-#	 gpg> showpref
-#	 [ultimate] (1).name <email>
-#	      Cipher: AES256, AES192, AES, 3DES
-#	      AEAD: OCB
-#	      Digest: SHA512, SHA384, SHA256, SHA224, SHA1
-#	      Compression: ZLIB, BZIP2, ZIP, Uncompressed
-#	      Features: MDC, AEAD, Keyserver no-modify
-#	 gpg> setpref AES256 AES192 AES 3DES SHA512 SHA384 SHA256 SHA224 SHA1 ZLIB BZIP2 ZIP
-#	 Set preference list to:
-#	      Cipher: AES256, AES192, AES, 3DES
-#	      AEAD:
-#	      Digest: SHA512, SHA384, SHA256, SHA224, SHA1
-#	      Compression: ZLIB, BZIP2, ZIP, Uncompressed
-#	      Features: MDC, Keyserver no-modify
-#	 Really update the preferences? (y/N) y
-#	 gpg> save
-#  pass init <key-id>
-
-gpg --import ~/pwd-keys/private.pgp
-gpg --import ~/pwd-keys/public.pgp
-rm -rf ~/pwd-keys
-
-# allow changes on the new machine - adds trust|5|save
-gpg --edit-key luizhgferreira@gmail.com
+# starting yabai
+yabai --start-service

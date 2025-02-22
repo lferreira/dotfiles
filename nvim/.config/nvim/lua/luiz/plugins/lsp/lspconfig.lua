5f17329e9631f5f1d9a1b1753f066f8f39ede441
luiz.ferreira <luizhgferreira@gmail.com>
2025-02-22 18:05:12 +0100

    adds karabiner

--- nvim/.config/nvim/lua/luiz/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/luiz/plugins/lsp/lspconfig.lua
@@ -1,57 +1,58 @@
 -- import lspconfig plugin safely
 local lspconfig_status, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status then
-	return
+  return
 end
 
 -- import cmp-nvim-lsp plugin safely
 local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not cmp_nvim_lsp_status then
-	return
+  return
 end
 
 local keymap = vim.keymap -- for conciseness
 
 -- enable keybinds only for when lsp server available
 local on_attach = function(client, bufnr)
-	-- keybind options
-	local opts = { noremap = true, silent = true, buffer = bufnr }
+  -- keybind options
+  local opts = { noremap = true, silent = true, buffer = bufnr }
 
-	-- set keybinds
-	keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
-	keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- got to declaration
-	keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- see definition and make edits in window
-	keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- go to implementation
-	keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- go to implementation
-	keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions
-	keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-	keymap.set("n", "<leader>rn", ":IncRename ", opts) -- smart rename
-	keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-	keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-	keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-	keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-	keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+  -- set keybinds
+  keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+  keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- got to declaration
+  keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- see definition and make edits in window
+  keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- go to implementation
+  keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- go to implementation
+  keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions
+  keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+  keymap.set("n", "<leader>rn", ":IncRename ", opts) -- smart rename
+  keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+  keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+  keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+  keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+  keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 end
 
 -- used to enable autocompletion (assign to every lsp server config)
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
-util = require("lspconfig/util")
+util = require "lspconfig/util"
 -- Since we installed lspconfig and imported it, we can reach
 -- gopls by lspconfig.gopls
 -- we can then set it up using the setup and insert the needed configurations
-lspconfig.gopls.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	cmd = { "gopls", "serve", "golangci-lint" },
-	filetypes = { "go", "gomod" },
-	root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-	settings = {
-		gopls = {
-			analyses = {
-				unusedparams = true,
-			},
-			staticcheck = false,
-		},
-	},
-})
+lspconfig.gopls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "gopls", "serve", "golangci-lint" },
+  filetypes = { "go", "gomod" },
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = false,
+      buildFlags = { "-tags", "integration" },
+    },
+  },
+}
